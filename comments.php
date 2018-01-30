b4b10c56e2646daeaa8db905514dea9f247c7cc2
Austin <nguyenau@oregonstate.edu>
2018-01-30 15:27:08 -0800

    checking

--- comments.php
+++
@@ -1,6 +0,0 @@
-<?php
-require("config.php");
-$conn = mysqli_connect(dbHost, dbUser, dbPass, dbName)
-$sql = 'SELECT * FROM Recipe';
-$result = mysqli_query($conn, $sql);
- ?>
--- connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-  // Define database connection constants
-  define('DB_HOST', 'classmysql.engr.oregonstate.edu');
-  define('DB_USER', 'cs340_kiefk');
-  define('DB_PASSWORD', 'Proschoolcool!');
-  define('DB_NAME', 'cs340_kiefk');
-?>
